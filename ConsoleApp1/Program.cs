86ab19e766251f9ff089ab963331e53157d02f7b
safiraauliafr <safiraafr@gmail.com>
2022-10-27 22:05:02 +0700

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,34 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+
+namespace TestProgram 
+{
+    class Emsisoft 
+    {
+        static void Main(string[] args)
+        {
+            
+            ChromeOptions options = new ChromeOptions();
+            options.AddArgument("--headless");
+            options.AddArgument("--disable-gpu");
+            string downloadPath = System.Environment.GetEnvironmentVariable("USERPROFILE") + "\\Downloads";
+            options.AddUserProfilePreference("safebrowsing.enabled", true);
+            options.AddUserProfilePreference("download.default_directory", downloadPath);
+            IWebDriver driver = new ChromeDriver(options);
+
+            driver.Navigate().GoToUrl("http://emsisoft.com/en/anti-malware-home");
+            driver.Manage().Window.Maximize();
+            IWebElement element = driver.FindElement(By.XPath("//a[text()='Alternative installation options']"));
+            element.Click();
+
+            IWebElement element2 = driver.FindElement(By.XPath("//a[text()='Web installer']"));
+            element2.Click();
+
+            Console.WriteLine(element2.Text);
+
+            //Close the browser
+            // driver.Close();
+        }
+    }
+}
